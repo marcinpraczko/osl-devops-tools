030cdcbb6915eba33a43efd82482965592e1b980
Marcin Praczko <marcin.praczko@gmail.com>
2018-08-04 15:12:04 +0100

    Use sudo prefix for running docker commands

--- docker/sphinx-tools/osl_env/bash.sh
+++ docker/sphinx-tools/osl_env/bash.sh
@@ -4,7 +4,7 @@
 
 function _osl_sphinx_tools_init_build_docs() {
     local VERSION="latest"
-    docker run --rm -it \
+    sudo docker run --rm -it \
         --hostname="osl-sphinx-tools-init" \
         -v $(pwd)/docs:/docs/sphinx-tools osl/sphinx-tools-init:"${VERSION}" \
             /usr/bin/make -C /docs/sphinx-tools/ html
@@ -17,9 +17,12 @@ function _osl_sphinx_tools() {
 #    local NEW_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 # --name=osl-sphinx-tools-"${NEW_ID}" \
 
-    docker run --rm -it \
+    sudo docker run --rm -it \
         --hostname="osl-sphinx-tools" \
         -p 80:80 \
-        -v `pwd`:/code osl/sphinx-tools:"${VERSION}" /bin/bash
+        -v `pwd`:/code \
+        -w '/code/docs' \
+        osl/sphinx-tools:"${VERSION}" /bin/bash
 }
 alias osl_run_sphinx_tools='_osl_sphinx_tools'
+
